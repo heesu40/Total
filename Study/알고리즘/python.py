5dd2a5afce89a9cf4bb912ab5e77952d4b5e53b3
heesu40 <heesu40@hanmail.net>
2020-06-26 22:56:21 +0900

    프로그래머스

--- Study/알고리즘/python.py
+++ Study/알고리즘/python.py
@@ -1,3 +1,12 @@
-a,b = map(input() , int(split(" ")))
-print(a // b)
-print(a % b)
+import heapq
+h = [5,4,6,8]
+k = 5
+heapq.heapify(h)
+L = len(h)
+f = heapq.heappop(h)
+for i in range(1, L):
+    s = heapq.heappop(h)
+    f = heapq.heappushpop(h, f + s * 2)
+    print("f의 값 {}".format(f))
+    if f >= k: print(i) 
+print(-1)
